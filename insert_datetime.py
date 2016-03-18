f75e39d58f055cd7b4287f27ced16369e9235b78
Eric Leclerc <eric.leclerc2@canada.ca>
2016-03-18 14:05:38 -0400

    format datetime (do not rely on OS for format)

--- insert_datetime.py
+++ insert_datetime.py
@@ -4,4 +4,4 @@ class InsertDatetimeCommand(sublime_plugin.TextCommand):
    def run(self, edit):
        sel = self.view.sel();
        for s in sel:
-           self.view.replace(edit, s, time.ctime())
+           self.view.replace(edit, s, time.strftime("%Y-%m-%d %H:%M"))
